02d960aec3268e75cf8eebf00f8c11fba9e2f05d
murikarox <eksi@kth.se>
2019-01-23 20:45:04 +0100

    namespace seperation, filesystem on the way

--- container.go
+++ container.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"syscall"
 )
 
@@ -38,15 +41,33 @@ func parent() {
 
 func child() { 
 	fmt.Printf("running %v as PID %d\n", os.Args[2], os.Getpid())
+	groups()
+
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+
+	must(syscall.Sethostname([]byte("container")))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+
 	if err := cmd.Run(); err != nil {
 		fmt.Println("ERROR", err)
 		os.Exit(1)
 	}
+}
+
+
+func groups() {
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups, "pids")
+	os.Mkdir(filepath.Join(pids, "container"), 0755)
+	must(ioutil.WriteFile(filepath.Join(pids, "container/pids.max"), []byte("20"), 0700))
+	// Removes the new cgroup in place after the container exits
+	must(ioutil.WriteFile(filepath.Join(pids, "container/notify_on_release"), []byte("1"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "container/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
 
 func must(err error) {
