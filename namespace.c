3b3eeb44752b8c4048cca443611f3da74f8b2b7f
murikarox <eksi@kth.se>
2019-01-23 20:29:56 +0100

    name space example in c

---
+++ namespace.c
@@ -0,0 +1,32 @@
+#define _GNU_SOURCE 
+#include <sched.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+int child(void *args)
+{
+    printf("pid as seen in the child process: %d\n", getpid());
+    system("bash");
+}
+
+
+int main()
+{
+    int namespaces = CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWIPC | CLONE_NEWNS | CLONE_NEWNET;
+    pid_t p = clone(child, malloc(4096) + 4096, SIGCHLD | namespaces, NULL);
+    if(p == -1) {
+        perror("clone error, whoops...");
+        exit(1);
+    }
+
+    printf("child process pid: %d \n ", p);
+    waitpid(p, NULL, 0);
+    return 0;
+}
